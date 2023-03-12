78013be5ad933cde7c58fd9a7738f5da91692a0a
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2023-03-12 19:52:13 +0530

    created diff docker-compose for dev and prod file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("using compose");
+  res.send("dev args");
 });
 
 const port = process.env.PORT || 3000;

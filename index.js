a7a17eaec275cecc949c8a30929351dbe036de0f
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2023-03-12 12:02:27 +0530

    created docker-compose file

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("learning docker");
+  res.send("using compose");
 });
 
 const port = process.env.PORT || 3000;

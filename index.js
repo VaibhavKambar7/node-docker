78d6c24e4dfa0cb6bef4740777f327c8c034dbe3
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2023-03-14 21:39:14 +0530

    Added config file to handle environment variables

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const {
+  MONGO_USER,
+  MONGO_PASSWORD,
+  MONGO_IP,
+  MONGO_PORT,
+} = require("./config/config");
 
 const mongoose = require("mongoose");
+
+mongoose
+  .connect(
+    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
+  )
+  .then(() => console.log("successfully connected to our db"))
+  .catch((e) => console.log(e));
 
 app.get("/", (req, res) => {
   res.send("mongo container added");

197666d3deaef26457d8ce1c3cfa6755ab75bfb9
VaibhavKambar7 <vaibhavkambar123@gmail.com>
2023-05-22 19:40:26 +0530

    building a crud application

--- index.js
+++ index.js
@@ -7,21 +7,25 @@ const {
   MONGO_PASSWORD,
   MONGO_IP,
   MONGO_PORT,
-} = require("./config/config");
+} = require("./config/config");  
+
+app.use(express.json())
+
+const postRouter = require("./routes/postRoutes") 
 
 const mongoose = require("mongoose");
 
-mongoose
-  .connect(
-    `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
-  )
-  .then(() => console.log("successfully connected to our db"))
-  .catch((e) => console.log(e));
+mongoose.connect(`mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`)
+  .then(() => console.log("successfully connected to our db"))  
+  .catch((e) => console.log(e)); 
 
 app.get("/", (req, res) => {
-  res.send("mongo container added");
-});
+  res.send("making blog app lettsg");
+}); 
 
+
+app.use("/api/v1/posts",postRouter)
+ 
 const port = process.env.PORT || 3000;
 
-app.listen(port, () => console.log(`listening on port : ${port}`));
+app.listen(port, () => console.log(`listening on port : ${port}`)); 
